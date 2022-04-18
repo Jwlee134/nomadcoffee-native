29784f5a460c635fd7b331c09ccc153a9d816533
Jaewon <sorhd134@gmail.com>
2022-04-18 09:41:54 +0900

    AppLoading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useAssets } from "expo-asset";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function App() {
+  const [loaded] = useFonts(Ionicons.font);
+  const [assets] = useAssets([require("./assets/coffee.jpg")]);
+
+  if (!loaded || !assets) return <AppLoading />;
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
